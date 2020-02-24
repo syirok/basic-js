49963fab39651088bed61714620a6646a26ab47b
Iryna Paliakova <40804746+syirok@users.noreply.github.com>
2020-02-24 16:00:00 +0300

    fix: add solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,28 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-};
+module.exports = function repeater(str, options) {
+
+str = String(str);
+
+let result = "";
+
+if (!options.repeatTimes) {options.repeatTimes = 1;}
+if (!options.separator) {options.separator = '+';}
+if (options.hasOwnProperty("addition")) {options.addition = String(options.addition);}
+    else {options.addition = "";}
+if (!options.additionRepeatTimes) {options.additionRepeatTimes = 1;}
+if (!options.additionSeparator) {options.additionSeparator = '|';}
+
+for (let i = 1; i <= options.repeatTimes; i++) {
+    result += str;
+    for (let j = 1; j <= options.additionRepeatTimes; j++) {
+        result += options.addition;
+        if (j < options.additionRepeatTimes) {
+            result += options.additionSeparator;
+        }
+    }
+    if (i<options.repeatTimes) {
+        result += options.separator;
+    }
+}
+return result;
+}
   
