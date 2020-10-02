e2c9e3d0a4ca2997d1f62ef63f506faa8ba8b0fd
Iryna Paliakova <40804746+syirok@users.noreply.github.com>
2020-10-02 21:56:25 +0300

    fix: add new solution

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,9 +1,12 @@
 module.exports = function countCats(backyard) {
-  let s = 0;
+  /*let s = 0;
   for (let i=0; i<backyard.length; i++) {
     for (let j=0; j<backyard[i].length; j++) {
       if (backyard[i][j] == "^^") {s += 1;}
     }
   }
-return s;
+return s;*/
+
+return backyard.flat().filter(el => el == '^^').length || 0;
+
 };
