7127c52f394ceece69c758a55ec1aeed9adec5b7
Iryna Paliakova <40804746+syirok@users.noreply.github.com>
2020-10-03 08:22:08 +0300

    fix: add new solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -3,12 +3,17 @@ module.exports = function createDreamTeam(members) {
 
  let team = "";
 
- for (let i in members) {
+ /*for (let i in members) {
    if (typeof members[i] == "string") {
-     members[i] = members[i].split(' ').join('');
+     members[i] = members[i].trim();
      team += members[i][0];
    }
- }
+ }*/
+
+ team = members
+  .filter(el => typeof el == "string")
+  .map(el => el.trim()[0])
+  .join('');
 
  return team.toUpperCase().split('').sort().join('');
 
